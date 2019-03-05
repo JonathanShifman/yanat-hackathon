c679989a54e6103a191a61cb10fab52effc2c840
Jonathan <jonathan.shifman@gmail.com>
2019-03-05 17:02:31 +0200

    Bugfix in finding closest snapshot

--- Server/index.js
+++ Server/index.js
@@ -5,28 +5,27 @@ app.use(cors());
 
 function get_flight_snapshots(req, res) {
     var MongoClient = require('mongodb').MongoClient;
-    let time = req.params['time'];
+    let time = +req.params['time'];
     let epsilon = 10;
     let timeLowerBound = time - epsilon;
     let timeUpperBound = time + epsilon;
 
     MongoClient.connect('mongodb://localhost:27017/recordings', function (err, client) {
         var db = client.db('recordings');
-        console.log(time);
         db.collection('mockFlights').find({'time': {$gte: timeLowerBound, $lte: timeUpperBound}}).toArray(function (err, result) {
             let closestSnapshots = {};
             let closestDiffs = {};
             for (let snapshot of result) {
                 let id = snapshot['id'];
                 let diff = Math.abs(snapshot['time'] - time);
-                if (closestDiffs[id] != null || diff < closestDiffs[id]) {
+                if (closestDiffs[id] == null || diff < closestDiffs[id]) {
                     closestDiffs[id] = diff;
                     closestSnapshots[id] = snapshot;
                 }
             }
 
             let finalResult = [];
-            for (let snapshot in Object.values(closestSnapshots)) {
+            for (let snapshot of Object.values(closestSnapshots)) {
                 finalResult.push(snapshot);
             }
             res.json({'snapshots': finalResult});
