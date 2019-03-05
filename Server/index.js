bc5e8e9cb1ebdfbd3d9184bd5fa5a9b011f332b9
Jonathan <jonathan.shifman@gmail.com>
2019-03-05 16:17:24 +0200

    Trying out filtering by time

--- Server/index.js
+++ Server/index.js
@@ -5,16 +5,18 @@ app.use(cors());
 
 function get_flight_snapshots(req, res) {
     var MongoClient = require('mongodb').MongoClient;
+    let time = req.params['time'];
 
     MongoClient.connect('mongodb://localhost:27017/recordings', function (err, client) {
         var db = client.db('recordings');
-        db.collection('mockFlights').find().toArray(function (err, result) {
+        db.collection('mockFlights').find({'time': 1}).toArray(function (err, result) {
             res.json({'snapshots': result});
         });
     });
 }
 
 app.get('/', (req, res) => get_flight_snapshots(req, res));
+app.get('/timespan/:time', (req, res) => get_flight_snapshots(req, res));
 
 const port = 5000;
 app.listen(port, () => console.log('Listening on port ' + port));
