f8cd70b79407186a41c3ad0626ce9198f99f5e77
Jonathan <jonathan.shifman@gmail.com>
2019-03-05 13:44:05 +0200

    Trying to connect to mongo

--- Server/index.js
+++ Server/index.js
@@ -2,19 +2,17 @@ const express = require('express');
 const app = express();
 
 function f(req, res) {
-    // var MongoClient = require('mongodb').MongoClient;
-    // MongoClient.connect('mongodb://localhost:27017/animals', function (err, client) {
-    //     if (err) throw err;
-    //
-    //     var db = client.db('animals');
-    //
-    //     db.collection('mammals').find().toArray(function (err, result) {
-    //         if (err) throw err;
-    //
-    //         console.log(result)
-    //     })
-    // });
-   return res.json({'a': 2});
+    var MongoClient = require('mongodb').MongoClient;
+    MongoClient.connect('mongodb://localhost:27017/recordings', function (err, client) {
+        if (err) throw err;
+
+        var db = client.db('recordings');
+        db.collection('flights').find().toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        })
+    });
+   return res.json({'a': 3});
 }
 
 app.get('/', (req, res) => f(req, res));
