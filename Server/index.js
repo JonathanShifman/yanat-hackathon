0a04c0af606c2351a5ce28a0445cc87d77315346
Jonathan <jonathan.shifman@gmail.com>
2019-03-05 14:56:15 +0200

    Trying to fix JSON response creation

--- Server/index.js
+++ Server/index.js
@@ -4,18 +4,14 @@ const app = express();
 function get_flight_snapshots(req, res) {
     var MongoClient = require('mongodb').MongoClient;
 
-    var dbClient;
     MongoClient.connect('mongodb://localhost:27017/recordings', function (err, client) {
-        dbClient = client;
+        var db = client.db('recordings');
+        db.collection('mockFlights').find().toArray(function (err, result) {
+            console.log(result.length);
+            res.json({'snapshots': result});
+        });
     });
-
-    var db = dbClient.db('recordings');
-    var queryResult;
-    db.collection('mockFlights').find().toArray(function (err, result) {
-        queryResult = result;
-    });
-
-   return res.json({'snapshots': queryResult});
+   // return res.json({'snapshots': queryResult});
 }
 
 app.get('/', (req, res) => get_flight_snapshots(req, res));
