da19aac7d84febb998c94701c5f7f1a02d6d07a4
Jonathan <jonathan.shifman@gmail.com>
2019-03-06 11:01:37 +0200

    Trying to fix

--- Server/index.js
+++ Server/index.js
@@ -84,7 +84,7 @@ function get_flight_snapshots_for_id(req, res) {
 function get_polygons(req, res) {
     get_db('polygons')
         .then(function (db) {
-            return db.collection('countries').find({}, {_id: 1, 'properties.NAME': 1}).toArray();
+            return db.collection('countries').find({}, {_id: 1}).toArray();
         })
         .then(function (result) {
             res.json({'countries': result});
@@ -96,10 +96,12 @@ function get_polygons(req, res) {
 
 function get_polygon(req, res) {
     let polygonId = +req.params['polygonId'];
+    var ObjectID = require('mongodb').ObjectID;
+
 
     get_db('polygons')
         .then(function (db) {
-            return db.collection('countries').find({_id: polygonId}).toArray();
+            return db.collection('countries').find({_id: new ObjectID(polygonId)}).toArray();
         })
         .then(function (result) {
             res.json({'countries': result});
